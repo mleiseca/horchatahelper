9a82edb8e81822513bdbf7cdff2002f467ae72b2
Michael Leiseca <mleiseca@gmail.com>
2012-11-18 20:09:10 -0600

    can now order. going to clean things up a bit

--- get_grub.rb
+++ get_grub.rb
@@ -4,12 +4,8 @@ require 'net/https'
 require 'rubygems'
 require 'xmlsimple'
 require 'json'
+require 'optparse'
 
-
-c = read_config
-
-API_KEY = c["apiKey"]
-HOST_NAME = c["grubhubHostname"]
 
 def fetch_url(urlstring)
   puts "Fetching url: #{urlstring}"
@@ -17,6 +13,7 @@ def fetch_url(urlstring)
   http = Net::HTTP.new(url.host, url.port)
 
   http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
   response = http.start { |h| h.post(url.request_uri, "") }
   data = XmlSimple.xml_in(response.body)
@@ -29,14 +26,36 @@ def fetch_url(urlstring)
     end
   end
   data
+end
+
+def fetch_geocode(location)
+
+  #"address": "111 W Washington ",
+  #    "address2": "Suite 2100",
+  #    "city"  : "Chicago",
+  #    "state"  : "IL",
+  #    "zip"   : "60604"
+
+  combined_address = [location["address"],location["address2"],location["city"], location["state"], location["zip"]]
+  combined_address.delete_if{|x| x == nil}
+  combined_address_string = combined_address.join(" ")
+
+  puts "Geocoding '#{combined_address_string}'"
+
+  #http://www.grubhub.com/services/utility/geocode?apiKey=&version=1&format=xml&combinedAddress=111+W+Washington+Chicago+IL
+
+  urlstring = "https://#{HOST_NAME}/services/utility/geocode?format=xml&version=1&apiKey=#{API_KEY}&combinedAddress=#{URI.encode(combined_address_string)}"
+
+  fetch_url(urlstring)
 
 end
 
 def fetch_restaurants_serving(location, item)
-  urlstring = "https://#{HOST_NAME}/services/search/lite/results?format=xml&apiKey=#{API_KEY}&lat=#{location[:lat]}&lng=#{location[:lng]}&menuSearchTerm=#{item}"
+  puts "Searching for #{item} at #{location}"
 
-  data = fetch_url(urlstring)
+  urlstring = "https://#{HOST_NAME}/services/search/lite/results?format=xml&apiKey=#{API_KEY}&lat=#{location['lat']}&lng=#{location['lng']}&menuSearchTerm=#{URI::encode(item)}"
 
+  fetch_url(urlstring)
 end
 
 
@@ -64,7 +83,7 @@ end
 def start_order_with_item(credentials, location, restaurant_id,pickup, generation_date, item, selections)
   urlstring = "https://#{HOST_NAME}/services/order/new?format=xml&apiKey=#{API_KEY}&restaurantId=#{restaurant_id}"
   urlstring += "&menuItemId=#{item['id']}"
-  urlstring += "&email=#{credentials[:email]}&password=#{credentials[:password]}"
+  urlstring += "&email=#{credentials['email']}&password=#{credentials['password']}"
   if selections
     urlstring += "&choiceOptions=" + selections.join(',')
   end
@@ -73,6 +92,7 @@ def start_order_with_item(credentials, location, restaurant_id,pickup, generatio
   if pickup
     urlstring += "&pickup=Y"
   end
+
 
   #todo
   urlstring += "&crossstreet=Madison"
@@ -98,7 +118,7 @@ end
 def apply_freegrub(credentials, order_check, freegrub_string)
 
   urlstring = "https://#{HOST_NAME}/services/order/giftcard/apply?format=xml&apiKey=#{API_KEY}"
-  urlstring += "&email=#{credentials[:email]}&password=#{credentials[:password]}"
+  urlstring += "&email=#{credentials['email']}&password=#{credentials['password']}"
   urlstring += "&giftCardCode=#{freegrub_string}"
   urlstring += "&orderId=#{order_check['order'][0]['id'][0]}"
 
@@ -107,7 +127,7 @@ end
 
 def finalize(credentials, order_check)
   urlstring = "https://#{HOST_NAME}/services/order/finalize?format=xml&apiKey=#{API_KEY}"
-  urlstring += "&email=#{credentials[:email]}&password=#{credentials[:password]}"
+  urlstring += "&email=#{credentials['email']}&password=#{credentials['password']}"
   urlstring += "&orderId=#{order_check['order'][0]['id'][0]}"
   #todo
   urlstring += "&payment=creditcard"
@@ -148,24 +168,74 @@ def display_check(order_check)
   end
 end
 
-def read_config
+def read_config()
   JSON.parse(File.read("config.json"))
 end
 
 if __FILE__==$0
 
-  #http://www.grubhub.com/services/utility/geocode?apiKey=&version=1&format=xml&combinedAddress=111+W+Washington+Chicago+IL
-  #location = HOLLAND_MI
-  search_term = 'Taco'
 
-  puts "Searching for #{search_term} at #{location}"
+  optparse = OptionParser.new do|opts|
+    # opts.banner = "Usage: check_mysql_stat_delta  ..."
 
-  search_response = fetch_restaurants_serving(location, search_term)
+    $options = {}
+    $options[:config] = read_config()
+    API_KEY = $options[:config]["apiKey"]
+    HOST_NAME = $options[:config]["grubhubHostname"]
 
-  matching_restaurants = data['restaurants'][0]['restaurant']
+
+
+    # This displays the help screen, all programs are
+    # assumed to have this option.
+    opts.on( '-l', '--location LOCATION_NAME', 'Select location from config file' ) do |location_name|
+      puts "got l: #{location_name}"
+      $options[:location]= $options[:config]["locations"][location_name]
+
+      unless $options[:location]
+        print "Could not find location: #{$options[:location]}"
+
+        exit
+      end
+    end
+    opts.on( '-t', '--term SEARCH_TERM', 'What do you want to eat?' ) do |search_term|
+      $options[:search_term]= search_term
+
+      unless $options[:search_term]
+        print "Enter a search term"
+        exit
+      end
+    end
+  end
+
+  optparse.parse!
+
+  if $options[:location] == nil
+    print "No location specified. Exiting"
+    exit
+  end
+
+  if $options[:location]["lat"]==nil|| $options[:location]["lng"]==nil
+    geocode_response = fetch_geocode($options[:location])
+
+
+    if geocode_response['geocode'].length == 1
+      lat = geocode_response['geocode'][0]["lat"][0]
+      lng = geocode_response['geocode'][0]["lng"][0]
+      puts "Had to geocode. You can skip this by adding lat:#{lat}, lng:#{lng} to your config for this address"
+
+      $options[:location]["lat"] = lat
+      $options[:location]["lng"] = lng
+    else
+      puts "Uh oh! Found #{geocode_response['geocode'].length} geocodes for this address. Fix your config with a distinct address"
+      exit
+    end
+  end
+  search_response = fetch_restaurants_serving($options[:location], $options[:search_term])
+
+  matching_restaurants = search_response['restaurants'][0]['restaurant']
 
   unless matching_restaurants
-    puts "No matching restaurants found"
+    puts "No matching restaurants found."
     exit
   end
 
@@ -173,21 +243,57 @@ if __FILE__==$0
 
   matching_restaurants.each do|restaurant|
 
+    puts "... Trying to match items at #{restaurant['name']} "
+
     menu = fetch_menu(restaurant['id'])
-    matching_items = extract_matching_item(menu, search_term)
+    matching_items = extract_matching_item(menu, $options[:search_term])
+
+    continue if ! matching_items
 
     matching_items.each do|item|
-      puts "...found item: #{item['name']}"
-      #menu['generation-date'][0]]
+      print "...found item: '#{item['name']}'. Ok? [y/N]: "
+
+      input = gets
+
+      if input.strip.downcase == 'y'
+        puts "Adding item to order..."
+        order_check = start_order_with_item($options[:config]["credentials"], $options[:location], restaurant['id'], true, menu['generation-date'][0], item, [])
+        display_check(order_check)
+
+        if $options[:config]["freegrubs"].length > 0
+          puts "Applying freegrub to order..."
+          order_check = apply_freegrub($options[:config]["credentials"],order_check, $options[:config]["freegrubs"][0])
+
+          display_check(order_check)
+        end
+
+        print "...place order? This will send money! [y/N]: "
+        input = gets
+        if input.strip.downcase == 'y'
+          order_check = finalize($options[:config]["credentials"], order_check)
+
+          puts "Final order check"
+          display_check(order_check)
+          exit
+        else
+          puts "Cold feet, eh? I'm outta here!"
+          exit
+        end
+      else
+        #  trying with the next item
+      end
     end
 
+    puts "No more matching items found at this restaurant. Trying the next one"
   end
+end
+puts "No more matching restaurants found."
 
-#  todo: yaml for
+
+#  todo: start using saved...
 #     - locations
 #     - payment options
 #     - login credentials
-#     - maximum amount to spend
 #  todo: command line input for
 #     - which location to use
 #     - an item to search for
@@ -196,5 +302,5 @@ if __FILE__==$0
 #  todo: general
 #     - loop to keep trying order
 #     - build an item with randomly selection (required only?) options
+#     - maximum amount to spend
 
-end
